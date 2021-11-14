721e7811f96d69e04b095bf1d991d32f22a66059
Alexandru Birta <80108383+AlexandruBirta@users.noreply.github.com>
2021-11-14 15:34:51 +0200

    Add files via upload

---
+++ src/main/java/ro/unibuc/fmi/ppcnewsletterproject/NewsletterProjectApplication.java
@@ -0,0 +1,13 @@
+package ro.unibuc.fmi.ppcnewsletterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsletterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsletterProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/unibuc/fmi/ppcnewsletterproject/NewsletterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.unibuc.fmi.ppcnewsletterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsletterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
